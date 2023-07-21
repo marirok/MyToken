c0f111b84cbd91b2120726778532dbdeab213080
marirok <95570657+marirok@users.noreply.github.com>
2023-07-21 09:11:48 +0200

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xfdebc34b3f8b4673e6a63593db062c7f78c769a1";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
